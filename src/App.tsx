2d9ed3df20d9b768cd50e6d2284984b2dabd39f8
EliHowland <elihowland25@gmail.com>
2024-04-06 22:26:46 +0000

    improving structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Header from "./components/Header";
+import React from "react";
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <HomePage></HomePage>
     </div>
   );
 }
