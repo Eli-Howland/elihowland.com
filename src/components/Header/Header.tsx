cc6635e21b90c1812aa87244a73cae39841a6191
Eli <elihowland25@gmail.com>
2024-04-14 22:43:49 +0000

    Header progress

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,27 +3,37 @@ import GitHub from "./GitHub";
 import Linkedin from "./Linkedin";
 
 const Header = () => {
+  const iconMaxWidth: number = 8
   return (
     <header
-      className="container-fluid text-center"
-      style={{ backgroundColor: "lightblue" }}
+      className="container-fluid text-white"
+      style={{ backgroundColor: "#3a3b3c" }}
     >
       {" "}
-      <div className="row border">
+      <div className="row p-2">
           <div className="col p-2">
-            <h1 style={{textAlign: "left"}}>
+            <h1 style={{textAlign: "left", fontFamily:"sans-serif", fontWeight:"700", marginLeft:"5%"}}>
               Eli Howland
             </h1>
           </div>
-      </div>
-      <div className="row p-1">
-        {" "}
-        <div className="col-1">
-          <GitHub width={50}></GitHub>
-        </div>
-        <div className="col-1">
-          <Linkedin width={50}></Linkedin>
-        </div>
+          <div className="col-5 p-2">
+            <h5 style={{fontFamily: "sans-serif", fontWeight:"300", textAlign:"center", marginTop:"2%"}}>
+              Project 1
+              <span style={{margin:"5%"}}></span>
+              Project 2
+              <span style={{margin:"5%"}}></span>
+              Project 3
+              <span style={{margin:"5%"}}></span>
+              Project 4
+              <span style={{margin:"5%"}}></span>
+              Project 5
+            </h5>
+          </div>
+          <div className="col p-3" style={{textAlign: "right"}}>
+            <GitHub width={iconMaxWidth}></GitHub>
+            <span style={{marginRight: "15px"}}></span>
+            <Linkedin width={iconMaxWidth}></Linkedin>
+          </div>
       </div>
     </header>
   );
