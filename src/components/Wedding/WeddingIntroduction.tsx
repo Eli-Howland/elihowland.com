1b73624f34cbab4e753e4229a2c531949165ce68
Eli <elihowland25@gmail.com>
2024-08-31 19:50:46 +0000

    Wedding progress

---
+++ src/components/Wedding/WeddingIntroduction.tsx
@@ -0,0 +1,9 @@
+import "../css/Wedding.css";
+
+const WeddingIntroduction = () => {
+  return (
+    <div className="wedding-introduction">HERES LIKE WAY MORE TEXT THAT</div>
+  );
+};
+
+export default WeddingIntroduction;
--- src/pages/Wedding.tsx
+++ src/pages/Wedding.tsx
@@ -1,5 +1,15 @@
+// import Header from "../components/Header/Header";
+import WeddingIntroduction from "../components/Wedding/WeddingIntroduction";
+import "../App.css";
+import "../components/css/Wedding.css";
+
 const WeddingPage = () => {
-  return <div className="default-page">HELLO WORLD</div>;
+  return (
+    <div className="default-page">
+      {/* <Header></Header> */}
+      <WeddingIntroduction></WeddingIntroduction>
+    </div>
+  );
 };
 
 export default WeddingPage;
